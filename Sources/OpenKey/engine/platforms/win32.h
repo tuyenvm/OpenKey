eb0e763b27fb33a649103b51b1fddfbe8524e495
Thanh Phu <git@thanhphu.net>
2024-01-07 04:39:35 +0900

    Dvorak support

--- Sources/OpenKey/engine/platforms/win32.h
+++ Sources/OpenKey/engine/platforms/win32.h
@@ -23,31 +23,31 @@
 
 #define KEY_EMPTY                               256
 #define KEY_A                                   0x41
-#define KEY_B                                   0x42
-#define KEY_C                                   0x43
-#define KEY_D                                   0x44
-#define KEY_E                                   0x45
-#define KEY_F                                   0x46
-#define KEY_G                                   0x47
-#define KEY_H                                   0x48
-#define KEY_I                                   0x49
-#define KEY_J                                   0x4A
-#define KEY_K                                   0x4B
-#define KEY_L                                   0x4C
+#define KEY_B                                   0x4E
+#define KEY_C                                   0x49
+#define KEY_D                                   0x48
+#define KEY_E                                   0x44
+#define KEY_F                                   0x59
+#define KEY_G                                   0x55
+#define KEY_H                                   0x4A
+#define KEY_I                                   0x47
+#define KEY_J                                   0x43
+#define KEY_K                                   0x56
+#define KEY_L                                   0x50
 #define KEY_M                                   0x4D
-#define KEY_N                                   0x4E
-#define KEY_O                                   0x4F
-#define KEY_P                                   0x50
-#define KEY_Q                                   0x51
-#define KEY_R                                   0x52
-#define KEY_S                                   0x53
-#define KEY_T                                   0x54
-#define KEY_U                                   0x55
-#define KEY_V                                   0x56
-#define KEY_W                                   0x57
-#define KEY_X                                   0x58
-#define KEY_Y                                   0x59
-#define KEY_Z                                   0x5A
+#define KEY_N                                   0x4C
+#define KEY_O                                   0x53
+#define KEY_P                                   0x52
+#define KEY_Q                                   0x58
+#define KEY_R                                   0x4F
+#define KEY_S                                   0xBA
+#define KEY_T                                   0x4B
+#define KEY_U                                   0x46
+#define KEY_V                                   0xBE
+#define KEY_W                                   0xBC
+#define KEY_X                                   0x42
+#define KEY_Y                                   0x54
+#define KEY_Z                                   0xBF
 
 #define KEY_1                                   0x31
 #define KEY_2                                   0x32
@@ -60,20 +60,20 @@
 #define KEY_9                                   0x39
 #define KEY_0                                   0x30
 
-#define KEY_LEFT_BRACKET                        0xDB //note
-#define KEY_RIGHT_BRACKET                       0xDD
+#define KEY_LEFT_BRACKET                        0xBD //note
+#define KEY_RIGHT_BRACKET                       0xBB
 
 #define KEY_LEFT_SHIFT                          160
 #define KEY_RIGHT_SHIFT                         161
 #define KEY_DOT                                 VK_OEM_PERIOD
 
 #define KEY_BACKQUOTE                           220
-#define KEY_MINUS                               189
-#define KEY_EQUALS                              187
-#define KEY_BACK_SLASH                          222
-#define KEY_SEMICOLON                           186
-#define KEY_QUOTE                               192
+#define KEY_MINUS                               222
+#define KEY_EQUALS                              221
+#define KEY_BACK_SLASH                          220
+#define KEY_SEMICOLON                           90
+#define KEY_QUOTE                               81
 #define KEY_COMMA                               VK_OEM_COMMA
-#define KEY_SLASH                               191
+#define KEY_SLASH                               219
 
 #endif //OPENKEY_WIN32_H
